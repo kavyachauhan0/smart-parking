f83d9ff66baaa5129f17ff41fec382d8ca109c04
kavya chauhan <chauhankavya76@gmail.com>
2026-03-10 15:25:56 +0530

    Add ParkingController with home endpo.int

---
+++ src/main/java/com/example/parking/controller/ParkingController.java
@@ -0,0 +1,13 @@
+package com.example.parking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ParkingController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
